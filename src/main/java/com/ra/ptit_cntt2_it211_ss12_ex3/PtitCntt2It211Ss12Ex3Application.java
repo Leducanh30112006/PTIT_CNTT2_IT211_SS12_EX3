533590234f718512c98366ae647a4e667a6fef9a
Leducanh30112006 <le1duc2anh3@gmail.com>
2026-05-27 22:50:31 +0700

    Initial commit

---
+++ src/main/java/com/ra/ptit_cntt2_it211_ss12_ex3/PtitCntt2It211Ss12Ex3Application.java
@@ -0,0 +1,13 @@
+package com.ra.ptit_cntt2_it211_ss12_ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtitCntt2It211Ss12Ex3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtitCntt2It211Ss12Ex3Application.class, args);
+    }
+
+}
